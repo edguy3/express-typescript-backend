a755b3ce62c180ea4b3c827258ac544066f9a14d
Ed Guy <edguy@eguy.org>
2023-12-05 11:41:22 -0500

    tweaks

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ app.get('/', (_: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
